6f2083df20ab2434b7749fb9986109bc1c2562fe
Antoine VINCENT <anvincen@paul-f5Br3s4.clusters.42paris.fr>
2023-08-22 15:48:48 +0200

    Finished the first shot of ex00 + begun ex01

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -1,36 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anvincen <anvincen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 12:39:16 by xuluu             #+#    #+#             */
-/*   Updated: 2023/08/22 12:53:26 by anvincen         ###   ########.fr       */
+/*   Updated: 2023/08/22 12:59:36 by anvincen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
 
-int	main(int argc, char **argv)
+class	Contact
 {
-	Output		message;
+	Public :
 
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (size_t i = 1; argv[i]; i++)
-		{
-			std::string	s(argv[i]);
-			for (std::string::iterator it = s.begin(); it != s.end(); it++)
-				*it = std::toupper(*it);
-			std::cout << s;
-			if (argv[i + 1] != NULL)
-				std::cout << " ";
-		}
-		std::cout << std::endl;
-	}
-	return (0);
+	char	*first_name;
+	char	*last_name;
+	char	*nick_name;
+	char	*phone_number;	
+	char	*darkest_secret;	
 }
