add647002af64d025e7fa9c226e1b2a28497c96b
anvincen <antvin100.av@gmail.com>
2023-09-06 16:49:09 +0200

    CPP01/ex02 => First shot everything seems ok

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 12:39:16 by anvincen          #+#    #+#             */
+/*   Updated: 2023/09/06 16:47:35 by antoine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string const	s = "HI THIS IS BRAIN";
+	std::string const	*stringPTR = &s;
+	std::string const	&stringREF = s;
+
+	std::cout << "string address is " << &s <<std::endl;
+	std::cout << "stringPTR address is " << stringPTR <<std::endl;
+	std::cout << "stringREF address is " << &stringREF <<std::endl;
+	std::cout << "string value is " << s <<std::endl;
+	std::cout << "stringPTR value is " << *stringPTR <<std::endl;
+	std::cout << "stringREF value is " << stringREF <<std::endl;
+	std::cout << "\nWell, that's all folks !\n\n" << std::endl;
+}
