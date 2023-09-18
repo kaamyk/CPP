9dc7058497a182a49ce24bcf8140d20d0547a908
anvincen <antvin100.av@gmail.com>
2023-09-18 23:37:39 +0200

    CPP02=>ex02 Working on the max function (the main from subject does not work)

---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 12:39:16 by anvincen          #+#    #+#             */
+/*   Updated: 2023/09/18 23:36:22 by antoine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main( void )
+{
+	Fixed		a;
+	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << "b == " << b << std::endl;
+	std::cout << "a == " << a << std::endl;
+
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	std::cout << a.max( a, b ) << std::endl;
+	return 0;
+}
