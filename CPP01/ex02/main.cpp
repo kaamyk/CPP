90456a4de89199bdf37acb4a6ec9431a2dd0272f
Antoine VINCENT <anvincen@MADE-f0Br5s4.clusters.42paris.fr>
2023-09-28 14:48:46 +0200

    CPP01/ex02 -> little changes before correction

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anvincen <anvincen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 12:39:16 by anvincen          #+#    #+#             */
-/*   Updated: 2023/09/28 14:41:57 by anvincen         ###   ########.fr       */
+/*   Updated: 2023/09/28 14:47:52 by anvincen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	main(void)
 	std::string const	&stringREF = s;
 
 	std::cout << "string address is " << &s <<std::endl;
-	std::cout << "stringPTR address is " << stringPTR <<std::endl;
-	std::cout << "stringREF address is \n" << &stringREF <<std::endl;
+	std::cout << "address in stringPTR is " << stringPTR << std::endl;
+	std::cout << "address in stringREF is " << &stringREF << std::endl << std::endl;
 	std::cout << "string value is " << s <<std::endl;
-	std::cout << "stringPTR value is " << *stringPTR <<std::endl;
-	std::cout << "stringREF value is " << stringREF <<std::endl;
+	std::cout << "value in stringPTR is " << *stringPTR << std::endl;
+	std::cout << "value in stringREF is " << stringREF << std::endl;
 	std::cout << "\n\tWell, that's all folks !\n" << std::endl;
 }
