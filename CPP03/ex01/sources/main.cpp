5c918910edc4be49d4ca3d7e2e8c1d309d9e1cb9
Antoine VINCENT <anvincen@made-f0Cr6s5.clusters.42paris.fr>
2023-10-10 18:14:36 +0200

    CPP03/ex01 => checks before correction

--- CPP03/ex01/sources/main.cpp
+++ CPP03/ex01/sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoine <antoine@student.42.fr>            +#+  +:+       +#+        */
+/*   By: anvincen <anvincen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 12:39:16 by anvincen          #+#    #+#             */
-/*   Updated: 2023/10/09 10:33:25 by antoine          ###   ########.fr       */
+/*   Updated: 2023/10/10 18:13:44 by anvincen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,18 @@ int	main( void )
 	ScavTrap	igor("igor");
 	ScavTrap	huguette("huguette");
 
-	toto.setAttackDamage(2);
-	igor.setAttackDamage(1);
-	huguette.setAttackDamage(1);
+	toto.setAttackDamage(200);
+	// igor.setAttackDamage(1);
+	// huguette.setAttackDamage(1);
 
 	toto.attack("huguette");	
 	huguette.takeDamage(toto.getAttackDamage());
 	igor.attack("huguette");
 	huguette.takeDamage(igor.getAttackDamage());
 	huguette.printAttributes();
-	igor.attack("toto");
-	toto.takeDamage(igor.getAttackDamage());
+	toto.attack("igor");
+	igor.takeDamage(toto.getAttackDamage());
+	igor.guardGate();
 	huguette.beRepaired(1);
 	huguette.printAttributes();
 	return ( 0 );
