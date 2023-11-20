77224e60dcfe335b933d19e15772c3204b562e7c
Antoine VINCENT <anvincen@made-f0Cr7s4.clusters.42paris.fr>
2023-11-20 18:13:25 +0100

    CPP07/ex00 => First shot seems ok

---
+++ CPP07/ex00/sources/main.cpp
@@ -0,0 +1,44 @@
+#include "../includes/Templates.hpp"
+
+int main( void )
+{
+    {
+        int a = 8;
+        int b = 6;
+
+        std::cout << "a == 8 && b == 6" << std::endl;
+        std::cout << max<int>(a, b) << std::endl;
+        std::cout << min<int>(a, b) << std::endl;
+        swap<int>(a, b);
+        std::cout << "a == " << a << std::endl;
+        std::cout << "b == " << b << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        char a = 'O';
+        char b = 'K';
+
+        std::cout << "a == 'O' && b == 'K'" << std::endl;
+        std::cout << max<char>(a, b) << std::endl;
+        std::cout << min<char>(a, b) << std::endl;
+        swap<char>(a, b);
+        std::cout << "a == " << a << std::endl;
+        std::cout << "b == " << b << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        int a = 2;
+        int b = 3;
+        ::swap( a, b );
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+        std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+        std::string c = "chaine1";
+        std::string d = "chaine2";
+        ::swap(c, d);
+        std::cout << "c = " << c << ", d = " << d << std::endl;
+        std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+        std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    }
+    return (0);
+}
