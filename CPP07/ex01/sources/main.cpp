58b3bdc832469763ac54898e9b3ec3c47df921f4
anvincen <antvin100.av@gmail.com>
2023-11-24 14:32:11 +0100

    CPP07/ex01 => Last check before correction

--- CPP07/ex01/sources/main.cpp
+++ CPP07/ex01/sources/main.cpp
@@ -13,9 +13,27 @@ int main( void )
             std::cout << "tab[" << i << "] == " << tab[i] << std::endl; 
         }
         std::cout << std::endl;
-        ::iter<int>(tab, l, &addOne<int>);
+        ::iter(tab, l, &addOne<int>);
         for (unsigned int i = 0; i < l; ++i)
             std::cout << "tab[" << i << "] == " << tab[i] << std::endl;
+        delete [] tab;
+    }
+    std::cout << "--------------------" << std::endl;
+    {
+	    std::srand(std::time(NULL));
+        char*    tab;
+        size_t  l = 10;
+
+        tab = new char[l];
+        for (unsigned int i = 0; i < l; ++i){
+            tab[i] = (rand() % 30) + 33;
+            std::cout << "tab[" << i << "] == " << tab[i] << std::endl; 
+        }
+        std::cout << std::endl;
+        ::iter(tab, l, &addOne<char>);
+        for (unsigned int i = 0; i < l; ++i)
+            std::cout << "tab[" << i << "] == " << tab[i] << std::endl;
+        delete [] tab;
     }
     return (0);
 }
