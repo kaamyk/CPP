cf13b67b0df012b726169bfad1dd6cbf48c51e4e
Antoine VINCENT <anvincen@made-f0Cr5s5.clusters.42paris.fr>
2023-12-01 11:34:00 +0100

    CPP07/ex00 => Last check before correction

--- CPP07/ex00/includes/Templates.hpp
+++ CPP07/ex00/includes/Templates.hpp
@@ -3,17 +3,17 @@
 
 #include <iostream>
 
-template<typename T> T max(T a, T b)
+template<typename T> T _max(T a, T b)
 {
     return (a > b ? a : b);
 }
 
-template<typename T> T min(T a, T b)
+template<typename T> T _min(T a, T b)
 {
     return (a < b ? a : b);
 }
 
-template<typename T> void swap(T& a, T& b)
+template<typename T> void _swap(T& a, T& b)
 {
     T  tmp;
 
--- CPP07/ex00/sources/main.cpp
+++ CPP07/ex00/sources/main.cpp
@@ -3,13 +3,13 @@
 int main( void )
 {
     {
-        int a = 8;
-        int b = 6;
+        long a = 8;
+        long b = 6;
 
         std::cout << "a == 8 && b == 6" << std::endl;
-        std::cout << max<int>(a, b) << std::endl;
-        std::cout << min<int>(a, b) << std::endl;
-        swap<int>(a, b);
+        std::cout << ::_max<long>(a, b) << std::endl;
+        std::cout << ::_min<long>(a, b) << std::endl;
+        ::_swap<long>(a, b);
         std::cout << "a == " << a << std::endl;
         std::cout << "b == " << b << std::endl;
         std::cout << std::endl;
@@ -19,25 +19,25 @@ int main( void )
         char b = 'K';
 
         std::cout << "a == " << a << " && b == " << b << std::endl;
-        std::cout << max<char>(a, b) << std::endl;
-        std::cout << min<char>(a, b) << std::endl;
-        swap<char>(a, b);
+        std::cout << ::_max<char>(a, b) << std::endl;
+        std::cout << ::_min<char>(a, b) << std::endl;
+        ::_swap<char>(a, b);
         std::cout << "a == " << a << " && b == " << b << std::endl;
         std::cout << std::endl;
     }
     {
         int a = 2;
         int b = 3;
-        ::swap( a, b );
+        ::_swap( a, b );
         std::cout << "a = " << a << ", b = " << b << std::endl;
-        std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-        std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+        std::cout << "min( a, b ) = " << ::_min( a, b ) << std::endl;
+        std::cout << "max( a, b ) = " << ::_max( a, b ) << std::endl;
         std::string c = "chaine1";
         std::string d = "chaine2";
-        ::swap(c, d);
+        ::_swap(c, d);
         std::cout << "c = " << c << ", d = " << d << std::endl;
-        std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-        std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+        std::cout << "min( c, d ) = " << ::_min( c, d ) << std::endl;
+        std::cout << "max( c, d ) = " << ::_max( c, d ) << std::endl;
     }
     return (0);
 }
